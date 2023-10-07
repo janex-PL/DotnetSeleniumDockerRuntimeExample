c3b93d819638157642156ed9dc7e641a12a924ef
janex-PL <kliszczjan@gmail.com>
2023-10-07 12:38:03 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM runtime-chrome:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["SeleniumChromeDocker.csproj", "./"]
+RUN dotnet restore "SeleniumChromeDocker.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "SeleniumChromeDocker.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "SeleniumChromeDocker.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "SeleniumChromeDocker.dll"]
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var chromeOptions = new ChromeOptions();
+chromeOptions.AddArguments("--headless","--disable-gpu",
+    "--no-sandbox", "--disable-dev-shm-usage"); // remember about the necessary arguments when running inside container! 
+
+while (true)
+{
+    Console.Write("Provide a version of Chrome browser you want to launch (type 'q' to quit):");
+    var input = Console.ReadLine();
+    
+    if (string.Equals(input, "q", StringComparison.OrdinalIgnoreCase))
+        break;
+    
+    chromeOptions.BrowserVersion = input;
+    var driver = new ChromeDriver(chromeOptions);
+    try
+    {
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        driver.Navigate().GoToUrl("https://www.whatsmybrowser.org/");
+        Console.WriteLine(driver.FindElement(By.TagName("h2")).Text);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    finally
+    {
+        driver.Quit();
+    }
+}
